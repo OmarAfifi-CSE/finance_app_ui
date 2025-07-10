db67e5e5a2404bcd13bc2869e663854e46730e23
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-10 20:55:39 +0300

    Add text style to app_styles

---
+++ lib/core/styling/app_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'app_fonts.dart';
+
+class AppStyles {
+  static const TextStyle primaryHeadlineStyle = TextStyle(
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+}
