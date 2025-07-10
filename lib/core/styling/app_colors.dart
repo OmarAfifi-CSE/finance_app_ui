85b2d34ea4e8802e5ca668f21c0c839b3f7807e4
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-10 21:08:41 +0300

    add more app colors and text styles

--- lib/core/styling/app_colors.dart
+++ lib/core/styling/app_colors.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors{
   static const Color primaryColor = Color(0xff617AFD);
   static const Color secondaryColor = Color(0xff8391A1);
+  static const Color blackColor = Color(0xff1F2C37);
+  static const Color grayColor = Color(0xff9CA4AB);
 
 }
--- lib/core/styling/app_styles.dart
+++ lib/core/styling/app_styles.dart
@@ -18,4 +18,16 @@ class AppStyles {
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
+  static TextStyle black16w600Style = TextStyle(
+    color: AppColors.blackColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle gray12w500Style = TextStyle(
+    color: AppColors.grayColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
 }
