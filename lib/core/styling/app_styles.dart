d68a9847d3f7f72be22f4c22f07071fb8d8efbd7
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-10 21:01:01 +0300

    add subtitles style

--- lib/core/styling/app_styles.dart
+++ lib/core/styling/app_styles.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'app_colors.dart';
 import 'app_fonts.dart';
 
 class AppStyles {
-  static const TextStyle primaryHeadlineStyle = TextStyle(
+  static TextStyle primaryHeadlineStyle = TextStyle(
+    color: AppColors.primaryColor,
     fontFamily: AppFonts.mainFontName,
-    fontSize: 30,
+    fontSize: 30.sp,
     fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle subtitlesStyle = TextStyle(
+    color: AppColors.secondaryColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
