dcd0f31114dbad35cecebd647099f35e28f00e13
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-21 04:24:20 +0300

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:finance_app_ui/core/routing/router_generation_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/styling/app_themes.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  // Ensure that the Flutter binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Then lock the orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
+      splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp.router(
           debugShowCheckedModeBanner: false,
